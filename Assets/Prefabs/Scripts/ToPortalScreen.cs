ce7c8950d2f564cb4d92f6164cc5ec53ff887433
Michelle <michieugwu@gmail.com>
2023-04-23 22:20:56 +0100

    Descriptive inventory ui with buttons

---
+++ Assets/Prefabs/Scripts/ToPortalScreen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using UnityEngine.UI;
+using TMPro;
+
+public class ToPortalScreen : MonoBehaviour
+{
+    public GameObject TimelineSelector;
+
+    void Start()
+    {
+        TimelineSelector.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // var TimelineSelector = GameObject.FindGameObjectWithTag("Timeline");
+            TimelineSelector.SetActive(true);
+        }
+    }
+    void OnTriggerExit()
+    {
+        TimelineSelector.SetActive(false);
+    }
+}
